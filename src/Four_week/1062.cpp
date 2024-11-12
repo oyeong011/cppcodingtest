2f0064a8c314b7bde35e2d3642ab9d2d42674e38
oyeong011 <oyeong011@gmail.com>
2024-11-12 13:08:44 +0900

    1062
    1. 이진수비교 연산자로 해결

--- src/Four_week/1062.cpp
+++ src/Four_week/1062.cpp
@@ -1,3 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, words[51];
+string s;
+int count(int mask) {
+    int cnt = 0;
+    for (int word : words) {
+        if(word && (word & mask) == word)cnt++;
+    }
+    return cnt;
+}
+int go(int index, int k, int mask) {
+    if (k < 0) return 0;
+    if (index == 26) return count(mask);
+    int ret = go(index+1, k-1, mask | (1 << index));
+    if (index != 'a'-'a' && index != 'n'-'a' && index != 't'-'a' && index != 'i'-'a' && index != 'c'-'a') {
+        ret = max(ret, go(index+1, k, mask));
+    }
+    return ret;
+}
+int main() {
+    cin >> n >> m;
+    for (int i=0; i<n; i++) {
+        cin >> s;
+        for (char str : s) {
+            words[i] |= (1 << (str - 'a'));
+        }
+    }
+    cout << go(0, m, 0) << '\n';
+    return 0;
+}
+// #include<bits/stdc++.h>
+// using namespace std;
+// int n, m, words[51];
+// const char a[] = {'a', 'n', 't', 'i', 'c'};
+// string s;
+// int count(int mask){
+//     int cnt = 0;
+//     for(auto word : words){
+//         if(word && (word & mask) == word)cnt++;
+//     }
+//     return cnt;
+// }
+// int go(int index, int k, int mask){
+//     if(k < 0)return 0;
+//     if(index == 26)return count(mask);
 //
-// Created by 권오영 on 24. 11. 11.
+//     int ret = go(index + 1, k - 1, mask | (1 << index));
+//     bool flag = false;
+//     for(auto b : a)if(index == b - 'a') flag = true;
 //
+//     if(flag) ret = max(ret, go(index+1, k, mask));
+//
+//     return ret;
+// }
+// int main(){
+//     cin >> n >> m;
+//     for(int i = 0; i < n; i++){
+//         cin >> s;
+//         for(char str : s){
+//             words[i] |= (1 << (str - 'a'));
+//         }
+//     }
+//     cout << go(0, m, 0) << "\n";
+//     return 0;
+// }
